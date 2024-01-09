36ac89436e276d911b28434e1d439c0c2dd706cc
snow4rider <matthew.t.mckenney@gmail.com>
2024-01-08 21:29:39 -0700

    finished the  chapter

---
+++ perimeter/shapes.go
@@ -0,0 +1,35 @@
+package shapes
+
+import (
+	"math"
+)
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Height float64
+	Base   float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Height + rectangle.Width)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
