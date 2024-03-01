cc0e2e038bdaa3c09edc5d5df5953db564964e43
snow4rider <matthew.t.mckenney@gmail.com>
2024-03-01 14:42:31 -0700

    Select working through the book

---
+++ Select/racer.go
@@ -0,0 +1,24 @@
+package racer
+
+import (
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string) (winner string) {
+	startA := time.Now()
+	http.Get(a)
+	aDuration := time.Since(startA)
+
+	startB := time.Now()
+	http.Get(b)
+	bDuration := time.Since(startB)
+
+	if aDuration < bDuration {
+		winner = a
+	} else {
+		winner = b
+	}
+
+	return winner
+}
